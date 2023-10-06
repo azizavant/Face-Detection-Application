8c3a9172e21d8b0bed0cf026fb52d4a9190910a3
azizavant <106331007+azizavant@users.noreply.github.com>
2023-10-06 19:40:28 +0600

    fixed file path

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/Face-Detection-Application/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/Face-Detection-Application/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/Face-Detection-Application/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/Face-Detection-Application/models')
 ]).then(startVideo)
 
 function startVideo() {
